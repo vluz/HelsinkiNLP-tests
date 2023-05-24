7a76212607def3bf5b4f314b0d4766ec45716e23
vluz <vluz@users.noreply.github.com>
2023-05-24 20:17:10 +0100

    Add files via upload

---
+++ trENPT.py
@@ -0,0 +1,19 @@
+from transformers import MarianMTModel, MarianTokenizer
+import streamlit as st
+
+model_name = 'Helsinki-NLP/opus-mt-tc-big-en-pt'
+model = MarianMTModel.from_pretrained(model_name)
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+
+def translate_text(text):
+    input_ids = tokenizer.encode(text, add_special_tokens=True, return_tensors="pt")
+    translated = model.generate(input_ids)
+    translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
+    return translated_text
+
+st.write("""# MarianMT Inglês para Português""")
+st.write("""Tradução vectorizada de EN para PT""")
+prompt = st.text_input("Texto em Inglês: ")
+if st.button('Send'): 
+    response = translate_text(prompt)
+    st.write("""Portugês: """, response)
---
+++ trUAPT.py
@@ -0,0 +1,18 @@
+from transformers import MarianMTModel, MarianTokenizer
+import streamlit as st
+
+model_name = 'Helsinki-NLP/opus-mt-uk-pt'
+model = MarianMTModel.from_pretrained(model_name)
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+
+def translate_text(text):
+    input_ids = tokenizer.encode(text, add_special_tokens=True, return_tensors="pt")
+    translated = model.generate(input_ids)
+    translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
+    return translated_text
+
+st.write("""# MarianMT UK to PT""")
+prompt = st.text_input("Введіть текст: ")
+if st.button('Send'): 
+    response = translate_text(prompt)
+    st.write("""Portugês: """, response)
