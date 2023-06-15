72e0a18081e0982b85d4f22ff0526706a7f4946b
vluz <vluz@users.noreply.github.com>
2023-06-15 20:27:48 +0100

    Update trUAPT.py

--- trUAPT.py
+++ trUAPT.py
@@ -1,18 +1,22 @@
 from transformers import MarianMTModel, MarianTokenizer
 import streamlit as st
+import warnings
 
-model_name = 'Helsinki-NLP/opus-mt-uk-pt'
-model = MarianMTModel.from_pretrained(model_name)
-tokenizer = MarianTokenizer.from_pretrained(model_name)
 
+@st.cache_resource
 def translate_text(text):
+    model_name = 'Helsinki-NLP/opus-mt-uk-pt'
+    model = MarianMTModel.from_pretrained(model_name)
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
     input_ids = tokenizer.encode(text, add_special_tokens=True, return_tensors="pt")
     translated = model.generate(input_ids)
     translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
     return translated_text
 
-st.write("""# UK to PT""")
+
+warnings.filterwarnings("ignore")
+st.write("""# Перекладіть українську на португальську""")
 prompt = st.text_input("Введіть текст: ")
-if st.button('Send'): 
+if st.button('Надіслати'):
     response = translate_text(prompt)
     st.write("""Portugês: """, response)
